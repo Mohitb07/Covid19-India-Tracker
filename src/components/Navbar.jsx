c9bfef9a6ff56595bc787936487a943d7a00e1f4
himanshugsn <himanshugsn30@gmail.com>
2021-05-06 21:21:17 +0530

    navbar adde

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import styles from './Navbar.module.css'
 function Navbar() {
     return (
         <div className={styles.container}>
-            COVID19 <span>INDIA</span>
+            <a href="/">COVID19 <span>INDIA</span></a>
         </div>
     )
 }
